ea4ca4865da8ed4b49052ee2d38120439953f2b3
Aiki Murayama <aikistream@gmail.com>
2019-09-01 02:10:02 +0900

    meta情報

--- pages/index.vue
+++ pages/index.vue
@@ -42,6 +42,18 @@ export default {
   },
   data(){
     return{
+      meta: [
+        {charset: 'utf-8'},
+        { name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        { hid: 'description', name: 'description', content: '148th開成祭の公式ホームページです。'},
+        { hid: 'og:site_name', property: 'og:site_name', content: '148th開成祭' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: 'https://kaisei-fes.com' },
+        { hid: 'og:title', property: 'og:title', content: '148th開成祭' },
+        { hid: 'og:description', property: 'og:description', content: '2019/9/21(土),22(日)開催。' },
+        { hid: 'og:image', property: 'og:image', content: 'https://kaisei-fes.com/assets/icon.JPG' },
+        { name: 'Twitter:Card', content: 'Summary_large_image' }
+      ],
       img1: access,
       img2: map,
       img3: sandan,
