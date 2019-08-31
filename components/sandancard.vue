f2936cfd442af6e2b8f0014c7dbbb3d78ad96ba1
Mencotton <mencotton0410@gmail.com>
2019-08-31 18:49:08 +0900

    Create component "sandancard"
    tap の同期問題も直った　ヨシ

---
+++ components/sandancard.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="card">
+    <div v-on:click="tap=!tap" class="grid-container">
+      <div class="thumbnail"><img src="" height="55px"></img></div>
+      <div class="place">{{ project.Place }}</div>
+      <div class="name">{{ project.Name }}</div>
+      <div class="description">{{ project.Description }}</div>
+      <div class="waitingtime">1<span class="minute">分</span></div>
+    </div>
+    <div class="grid-container2">
+      <div v-if=tap class="buttons map"><p>マップを見る</p></div>
+      <div v-if=tap class="buttons details"><p>詳しく</p></div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "sandancard",
+        props: ['project'],
+        data() {
+            return {
+                tap: false,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  .card {
+    margin: 10px 0;
+    border: solid 0.5px #828282;
+    border-radius: 10px;
+    background-color: #F8F9FA;
+    padding: 10px 10px;
+    align-items: center;
+  }
+
+
+  .grid-container {
+    display: grid;
+    grid-template-columns: 55px auto 45px;
+    grid-template-rows: 17px 25px 20px;
+  }
+
+  .thumbnail {
+    grid-column: 1;
+    grid-row: 1 / 4;
+  }
+
+  .place {
+    font-size: 13px;
+    color: #495057;
+    grid-column: 2 / 3;
+    grid-row: 1;
+  }
+
+  .name {
+    font-size: 19px;
+    font-weight: bold;
+    grid-column: 2 / 3;
+    grid-row: 2;
+  }
+
+  .description {
+    font-size: 13px;
+    color: #912A55;
+    grid-column: 2 / 3;
+    grid-row: 3;
+  }
+
+  .waitingtime {
+    font-size: 30px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+
+    grid-column: 3;
+    grid-row: 1 / 4;
+  }
+
+  .minute {
+    font-size: 15px;
+    margin-top: 10px;
+  }
+
+  .grid-container2 {
+    display: grid;
+    grid-template-columns: 49.5% 1% 49.5%;
+    margin: revert;
+  }
+
+  .map {
+    grid-column: 1/2;
+  }
+
+  .details {
+    grid-column: 3/4;
+  }
+</style>
--- pages/sandan.vue
+++ pages/sandan.vue
@@ -23,27 +23,14 @@
 
     <v-app>
       <v-content>
-        <div v-for="(project) in projects"
-             v-bind:datasrc="project.picture"
-             v-on:click="tap=!tap"
-             class="card">
-          <v-avatar>
-            <div class="grid-container">
-              <div class="thumbnail"><img src="" height="55px"></img></div>
-              <div class="place">{{ project.Place }}</div>
-              <div class="name">{{ project.Name }}</div>
-              <div class="description">{{ project.Description }}</div>
-              <div class="waitingtime">1<span class="minute">分</span></div>
-            </div>
-            <div class="grid-container2">
-              <div v-if=tap class="buttons map"><p>マップを見る</p></div>
-              <div v-if=tap class="buttons details"><p>詳しく</p></div>
-            </div>
-          </v-avatar>
-        </div>
+        <SandanCard v-for="(project) in projects"
+                    v-bind:key="project.id"
+                    v-bind:project="project"
+                    v-bind:datasrc="project.picture"
+        >
+        </SandanCard>
       </v-content>
     </v-app>
-
 
 
     <!--一応残したやつ-->
@@ -68,11 +55,13 @@
 
 <script>
     import algoliasearch from 'algoliasearch/lite';
-    import Header from '~/components/Myheader.vue'
+    import Header from '~/components/Myheader.vue';
+    import SandanCard from '~/components/sandancard.vue';
 
     export default {
         components: {
-          Header
+            Header,
+            SandanCard
         },
         name: 'project',
         data: function () {
@@ -80,7 +69,7 @@
                 projects: [],
                 index: null,
                 search_text: '',
-                tap: false,
+                // tap: false,
             };
         },
         created: function () {
@@ -110,7 +99,7 @@
 </script>
 
 <style>
-  .search-box-title{
+  .search-box-title {
     margin-top: 10px;
     background: #912A55;
     color: white;
@@ -120,42 +109,51 @@
     text-align: center;
     border-radius: 5px 5px 0 0;
   }
-  .search-box{
+
+  .search-box {
     width: 100%;
     height: 35px;
     border-radius: 0 0 5px 5px;
   }
-  .focus-on{
+
+  .focus-on {
     background: #F8F9FA;
     display: grid;
     grid-template-columns: 33% 16.5% 16.5% 33%;
     grid-template-rows: 50px 50px;
     border-radius: 0 0 5px 5px;
   }
-  .buttons{
+
+  .buttons {
     margin: 5px;
   }
-  .tenji{
+
+  .tenji {
     grid-column: 1/2;
     grid-row: 1/2;
 
   }
-  .sanka{
+
+  .sanka {
     grid-column: 2/4;
     grid-row: 1/2;
   }
-  .eiga{
+
+  .eiga {
     grid-column: 4/5;
     grid-row: 1/2;
   }
-  .perform{
+
+  .perform {
     grid-column: 1/3;
     grid-row: 2/3;
   }
-  .eats{
+
+  .eats {
     grid-column: 3/5;
     grid-row: 2/3;
   }
+
   .img {
     border-radius: 27.5px;
   }
@@ -177,76 +175,4 @@
     grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
     grid-gap: 10px;
   }
-
-  /*--カード--*/
-  .card {
-    margin: 10px 0;
-    border: solid 0.5px #828282;
-    border-radius: 10px;
-    background-color: #F8F9FA;
-    padding: 10px 10px;
-    align-items: center;
-  }
-
-  .grid-container {
-    display: grid;
-    grid-template-columns: 55px auto 45px;
-    grid-template-rows: 17px 25px 20px;
-  }
-
-  .thumbnail {
-    grid-column: 1;
-    grid-row: 1 / 4;
-  }
-
-  .place {
-    font-size: 13px;
-    color: #495057;
-    grid-column: 2 / 3;
-    grid-row: 1;
-  }
-
-  .name {
-    font-size: 19px;
-    font-weight: bold;
-    grid-column: 2 / 3;
-    grid-row: 2;
-  }
-
-  .description {
-    font-size: 13px;
-    color: #912A55;
-    grid-column: 2 / 3;
-    grid-row: 3;
-  }
-
-  .waitingtime {
-    font-size: 30px;
-    display: flex;
-    align-items: center;
-    text-align: center;
-
-    grid-column: 3;
-    grid-row: 1 / 4;
-  }
-  .minute{
-    font-size: 15px;
-    margin-top: 10px;
-  }
-
-  .grid-container2 {
-    display: grid;
-    grid-template-columns: 49.5% 1% 49.5%;
-    margin: revert;
-  }
-
-  .map {
-    grid-column: 1/2;
-  }
-
-  .details {
-    grid-column: 3/4;
-  }
-
-
 </style>
