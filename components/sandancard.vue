b88f1344e5cdd91f70d2fed82a92998d3c1b9882
Mencotton <mencotton0410@gmail.com>
2019-09-02 22:24:58 +0900

    Hide sandancard thumbnail

--- components/sandancard.vue
+++ components/sandancard.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="card">
     <div v-on:click="tap=!tap" class="grid-container">
-      <div class="thumbnail"><img src="" height="55px"></img></div>
+     <!-- <div class="thumbnail"><img :src="require('~/assets'+'/images/4-6.jpeg')" height="55px"></div> -->
       <div class="place">{{ project.Place }} {{project.Room}}</div>
       <div class="name">{{ project.Name }}</div>
       <div class="description">{{ project.Description }}</div>
       <div class="waitingtime">1<span class="minute">分</span></div>
     </div>
-    <div class="grid-container2">
-      <div v-show=tap class="buttons map"><p>マップを見る</p></div>
-      <div v-show=tap class="buttons details"><p>詳しく</p></div>
+    <div v-show=tap class="grid-container2">
+      <div class="buttons map"><p>マップを見る</p></div>
+      <div class="buttons details"><p>詳しく</p></div>
     </div>
-    <div v-show="tap" class="describe-more">
-      <div class="about-this"><img src="~/assets/sectionmark_alone.svg"><p>この団体について</p></div>
-      <div>{{project.Sandan-introduction}}</div>
-      <div class="about-this"><img src="~/assets/sectionmark_alone.svg"><p>Image</p></div>
+    <div v-show=tap class="describe-more">
+      <div class="about-this"><img src="~/assets/sectionmark_alone.svg">
+        <p>この団体について</p>
+      </div>
+      <div>{{project.Description}}</div>
+      <!--
+      <div class="about-this"><img src="~/assets/sectionmark_alone.svg">
+        <p>Image</p></div>
       <div>{{project.Thumbnail}}</div>
-      <div class="about-this"><img src="~/assets/sectionmark_alone.svg"><p>宣伝...</p></div>
-      <div>{{project.Sandan-introduction}}</div>
-
+      -->
     </div>
   </div>
 </template>
@@ -31,7 +33,7 @@
             return {
                 tap: false,
             }
-        }
+        },
     }
 </script>
 
@@ -94,46 +96,53 @@
     margin-top: 10px;
   }
 
-@media screen and (min-width: 750px) {
-  .card{
-    margin: 5px 0;
+  @media screen and (min-width: 750px) {
+    .card {
+      margin: 5px 0;
+    }
+
+    .grid-container2 {
+      display: grid;
+      grid-template-columns: 49.5% 1% 49.5%;
+    }
+
+    .map {
+      grid-column: 1/2;
+    }
+
+    .details {
+      grid-column: 3/4;
+    }
   }
-  .grid-container2 {
-    display: grid;
-    grid-template-columns: 49.5% 1% 49.5%;
-  }
-  .map {
-    grid-column: 1/2;
-  }
-  .details {
-    grid-column: 3/4;
-  }
-}
 
   @media screen and (max-width: 750px) {
-    .grid-container2{
+    .grid-container2 {
       grid-template-columns: 100%;
       grid-template-rows: 40px 40px;
       grid-gap: 10px;
     }
+
     .map {
       grid-row: 1/2;
     }
+
     .details {
       grid-row: 2/3;
     }
   }
 
-  .describe-more{
+  .describe-more {
     margin: 0 10px;
   }
-  .about-this{
+
+  .about-this {
     display: flex;
     flex-direction: row;
     font-size: 18px;
     padding-top: 7px;
   }
-  .about-this >p{
+
+  .about-this > p {
     position: relative;
     left: 5px;
   }
